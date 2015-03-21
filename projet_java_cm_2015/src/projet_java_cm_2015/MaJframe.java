7697490e5ea8b9ac5a2c39700c32cb5324f4f45a
mackaBah <macati1976@hotmail.com>
2015-03-21 20:30:52 +0100

    Signed-off-by: mackaBah <macati1976@hotmail.com>

---
+++ projet_java_cm_2015/src/projet_java_cm_2015/MaJframe.java
@@ -0,0 +1,58 @@
+package projet_java_cm_2015;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import java.awt.Color;
+import java.awt.GridBagLayout;
+import java.awt.FlowLayout;
+import javax.swing.JButton;
+import javax.swing.JCheckBox;
+import javax.swing.JList;
+import javax.swing.JLabel;
+import java.awt.Rectangle;
+
+public class MaJframe extends JFrame {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					MaJframe frame = new MaJframe();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public MaJframe() {
+		getContentPane().setBounds(new Rectangle(47, 0, 36, 33));
+		setBackground(Color.GRAY);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		/**Quand je vais cliquer sur la croix la fenêtre dois
+		*se fermer
+		*/
+		setBounds(100, 100, 450, 300);
+		getContentPane().setLayout(new BorderLayout(0, 0));
+		
+		JLabel lblCancel = new JLabel("cancel");
+		getContentPane().add(lblCancel, BorderLayout.NORTH);
+	}
+
+}
